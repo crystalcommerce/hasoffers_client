371911446bd5ff6a4258837acf735dd9e8bd8b34
Michael Xavier <michael@michaelxavier.net>
2012-10-19 13:12:40 -0700

    Evidently attr_accessor is variadic

--- lib/has_offers/models/advertiser_signup.rb
+++ lib/has_offers/models/advertiser_signup.rb
@@ -1,18 +1,18 @@
 module HasOffers
   module Models
     class AdvertiserSignup
-      attr_accessor :company
-      attr_accessor :city
-      attr_accessor :phone
-      attr_accessor :region
-      attr_accessor :address1
-      attr_accessor :address2
-      attr_accessor :country
-      attr_accessor :zipcode
-      attr_accessor :email
-      attr_accessor :first_name
-      attr_accessor :last_name
-      attr_accessor :password
+      attr_accessor :company,
+                    :city,
+                    :phone,
+                    :region,
+                    :address1,
+                    :address2,
+                    :country,
+                    :zipcode,
+                    :email,
+                    :first_name,
+                    :last_name,
+                    :password
 
       def to_hash
         {
