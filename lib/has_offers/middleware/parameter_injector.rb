608715754a1a4af0eddc114ab2e42308fcbb18d4
Michael Xavier <michael@michaelxavier.net>
2012-10-19 15:13:04 -0700

    Fix injector to work on get/post params

--- lib/has_offers/middleware/parameter_injector.rb
+++ lib/has_offers/middleware/parameter_injector.rb
@@ -1,22 +1,35 @@
 module HasOffers
   module Middleware
     class ParameterInjector < ::Faraday::Middleware
-      #extend Forwardable
-      #def_delegators :'Faraday::Utils', :parse_query, :build_query
+      extend Forwardable
+      def_delegators :'Faraday::Utils', :parse_query, :build_query
 
       def call(env)
-        env[:body].merge!(param_overrides)
+        case env[:method]
+        when :post, :put then merge_post_params(env)
+        else                  merge_url_params(env)
+        end
 
         @app.call(env)
       end
 
     private
 
+      def merge_post_params(env)
+        env[:body] ||= {}
+        env[:body].merge!(param_overrides)
+      end
+
+      def merge_url_params(env)
+        new_params = query_params(env[:url]).merge(param_overrides)
+
+        env[:url].query = build_query(new_params)
+      end
+
       def param_overrides
         raise NotImplementedError
       end
 
-      #TODO: deprecated?
       def query_params(url)
         if url.query.nil? or url.query.empty?
           {}
